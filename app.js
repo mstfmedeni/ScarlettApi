87c5fe26fb6a569821caead4b5e51d69b02cc7a6
mustafa MEDENi <mustafa.medeni@hotmail.com>
2017-10-28 23:47:09 +0300

    ilk repo

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let http = require('http');
+let server = require('./lib/server');
+
+const PORT = process.env.port || 3000;
+
+server.set('port', PORT);
+let app = http.createServer(server);
+
+app.listen(PORT, () => {
+  console.log('Aplikasi udah jalan.....');
+});
